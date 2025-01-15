53f150f959c9d6cbd79c41c99dc729d676de2fed
EliDimi <2275eli@gmail.com>
2025-01-15 18:56:00 +0200

    Adding the see menu function
    also correcting logic for the manager menu

--- Restaurant Software.cpp
+++ Restaurant Software.cpp
@@ -7,7 +7,6 @@ void clearConsole() {
     cout << "\033[J"; // Clears the console
 }
 
-
 void startingMessages(char& role) {
     cout << "Welcome!" << endl;
     cout << "Today is 01.01.2025" << endl;
@@ -32,68 +31,118 @@ void startingMessages(char& role) {
     }
 }
 
-void printOptionsForManager() {
-    cout << "Select an option:" << endl;
-    cout << "1) See menu" << endl;
-    cout << "2) Make an order" << endl;
-    cout << "3) Remove an order" << endl;
-    cout << "4) See past orders" << endl;
-    cout << "5) See past oders list" << endl;
-    cout << "6) See how much products are left" << endl;
-    cout << "7) Write- off a product" << endl;
-    cout << "8) Stock a product" << endl;
-    cout << "9) See daily revenue" << endl;
-    cout << "10) Generate a daily revenue" << endl;
-    cout << "11) See all revenues" << endl;
-    cout << "12) Add a new item in the menu" << endl;
-    cout << "13) Remove an item from the menu" << endl;
-    cout << "14) See all options " << endl;
-    cout << "15) Exit" << endl;
-
-    int option;
-    while (true) {
-        cout << "Choosing: ";
-        cin >> option;
-        if (option < 1 || option > 15) {
-            cout << "Invalid option! Select again!" << endl;
-        }
-        else {
-            clearConsole();
-            break;
-        }
+void readAndPrintMenu(const char* Menu) {
+    ifstream file(Menu);
+    if (!file.is_open()) {
+        cout << "Error: Unable to open file." << endl;
+        return;
     }
 
-    switch (option) {
-    case 1:
-        
-    case 2:
+    file.seekg(0, ios::end);
+    size_t fileSize = file.tellg();
+    file.seekg(0, ios::beg);
+    file.clear();
 
-    case 3:
-        
-    case 4:
-        
-    case 5:
+    char* fileContent = new char[fileSize + 1];
 
-    case 6:
-        
-    case 7:
-        
-    case 8:
+    file.getline(fileContent, fileSize, '\0');
 
-    case 9:
-    
-    case 10:
+    cout << "The menu is: " << endl;
+    cout << fileContent << endl;
+    cout << endl;
 
-    case 11:
-        
-    case 12:
-        
-    case 13:
 
-    case 14:
-        printOptionsForManager();
-    case 15:
-        return;
+    delete[] fileContent;
+    file.close();
+}
+
+void seeMenu() {
+    const char* menu = "Menu.txt";
+    readAndPrintMenu(menu);
+}
+
+void printOptionsForManager() {
+    while (true) {
+        cout << "Select an option:" << endl;
+        cout << "1) See menu" << endl;
+        cout << "2) Make an order" << endl;
+        cout << "3) Remove an order" << endl;
+        cout << "4) See past orders" << endl;
+        cout << "5) See past oders list" << endl;
+        cout << "6) See how much products are left" << endl;
+        cout << "7) Write- off a product" << endl;
+        cout << "8) Stock a product" << endl;
+        cout << "9) See daily revenue" << endl;
+        cout << "10) Generate a daily revenue" << endl;
+        cout << "11) See all revenues" << endl;
+        cout << "12) Add a new item in the menu" << endl;
+        cout << "13) Remove an item from the menu" << endl;
+        cout << "14) See all options " << endl;
+        cout << "15) Exit" << endl;
+
+        int option;
+        bool validOption = false;
+
+        while (!validOption) {
+            cout << "Choosing: ";
+            cin >> option;
+
+            if (option < 1 || option > 15) {
+                cout << "Invalid option! Select again!" << endl;
+            }
+            else {
+                validOption = true;
+            }
+        }
+
+        clearConsole();
+
+        switch (option) {
+        case 1:
+            seeMenu();
+
+        case 2:
+            // makeOrder();
+            break;
+        case 3:
+            // removeOrder();
+            break;
+        case 4:
+            // seePastOrders();
+            break;
+        case 5:
+            // seePastOrdersList();
+            break;
+        case 6:
+            // seeProductsLeft();
+            break;
+        case 7:
+            // writeOffProduct();
+            break;
+        case 8:
+            // stockProduct();
+            break;
+        case 9:
+            // seeDailyRevenue();
+            break;
+        case 10:
+            // generateDailyRevenue();
+            break;
+        case 11:
+            // seeAllRevenues();
+            break;
+        case 12:
+            // addNewItem();
+            break;
+        case 13:
+            // removeItemFromMenu();
+            break;
+        case 14:
+            printOptionsForManager();
+            break;
+        case 15:
+            return;
+        }
     }
 }
 
@@ -128,7 +177,7 @@ void printOptionsForWaiter() {
 
         switch (option) {
         case 1:
-            //seeMenu();
+            seeMenu();
             break;
         case 2:
             //makeOrder();
