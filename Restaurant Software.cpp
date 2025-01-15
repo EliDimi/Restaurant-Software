6d433d2ccf9ae38eb5a06a524f270e6a4a6efda9
EliDimi <2275eli@gmail.com>
2025-01-15 15:12:36 +0200

    Building logic of choosing an action
    Currently "See all options" logic is functioning. I have two logic for choosing an action - will test them further and see which one is better.

--- Restaurant Software.cpp
+++ Restaurant Software.cpp
@@ -47,28 +47,111 @@ void printOptionsForManager() {
     cout << "11) See all revenues" << endl;
     cout << "12) Add a new item in the menu" << endl;
     cout << "13) Remove an item from the menu" << endl;
-    cout << "14)See all options " << endl;
+    cout << "14) See all options " << endl;
+    cout << "15) Exit" << endl;
 
     int option;
-    cout << "Choosing: ";
-    cin >> option;
+    while (true) {
+        cout << "Choosing: ";
+        cin >> option;
+        if (option < 1 || option > 15) {
+            cout << "Invalid option! Select again!" << endl;
+        }
+        else {
+            clearConsole();
+            break;
+        }
+    }
 
+    switch (option) {
+    case 1:
+        
+    case 2:
+
+    case 3:
+        
+    case 4:
+        
+    case 5:
+
+    case 6:
+        
+    case 7:
+        
+    case 8:
+
+    case 9:
+    
+    case 10:
+
+    case 11:
+        
+    case 12:
+        
+    case 13:
+
+    case 14:
+        printOptionsForManager();
+    case 15:
+        return;
+    }
 }
 
 void printOptionsForWaiter() {
-    cout << "Select an option:" << endl;
-    cout << "1) See menu" << endl;
-    cout << "2) Make an order" << endl;
-    cout << "3) Remove an order" << endl;
-    cout << "4) See past orders" << endl;
-    cout << "5) See past oders list" << endl;
-    cout << "6) See daily revenue" << endl;
-    cout << "7) See all options" << endl;
+    while (true) {
+        cout << "Select an option:" << endl;
+        cout << "1) See menu" << endl;
+        cout << "2) Make an order" << endl;
+        cout << "3) Remove an order" << endl;
+        cout << "4) See past orders" << endl;
+        cout << "5) See past orders list" << endl;
+        cout << "6) See daily revenue" << endl;
+        cout << "7) See all options" << endl;
+        cout << "8) Exit" << endl;
 
-    int option;
-    cout << "Choosing: ";
-    cin >> option;
-    
+        int option;
+        bool validOption = false;
+
+        while (!validOption) {
+            cout << "Choosing: ";
+            cin >> option;
+
+            if (option < 1 || option > 8) {
+                cout << "Invalid option! Select again!" << endl;
+            }
+            else {
+                validOption = true;
+            }
+        }
+
+        clearConsole();
+
+        switch (option) {
+        case 1:
+            //seeMenu();
+            break;
+        case 2:
+            //makeOrder();
+            break;
+        case 3:
+            //removeOrder();
+            break;
+        case 4:
+            //seePastOrders();
+            break;
+        case 5:
+            //seePastOrdersList();
+            break;
+        case 6:
+            //seeDailyRevenue();
+            break;
+        case 7:
+            continue;
+        case 8:
+            // Exit the function
+            return;
+        }
+    }
 }
 
 int main() {
