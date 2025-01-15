563691986103250965c975b61f6b64b4e31c6502
EliDimi <2275eli@gmail.com>
2025-01-15 11:56:34 +0200

    Optimize selecting role function

--- Restaurant Software.cpp
+++ Restaurant Software.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-const int ROLEMAXSIZE = 2;
-
-void startingMessages(char* role) {
-    if (role == nullptr) {
-        cout << "Error! You have problem with role!" << endl;
-        return;
-    }
+void startingMessages(char& role) {
     cout << "Welcome!" << endl;
     cout << "Today is 01.01.2025" << endl;
     cout << "Please select an option:" << endl;
     cout << "A - Manager or B - Waiter" << endl;
     while (true) {
         cout << "I am: ";
-        cin.getline(role, ROLEMAXSIZE);
-        if (role[0] == 'A' || role[0] == 'a') {
+        cin >> role;
+        if (role == 'A' || role == 'a') {
             cout << "You selected: Manager" << endl;
             break;
         }
-        else if (role[0] == 'B' || role[0] == 'b') {
+        else if (role == 'B' || role == 'b') {
             cout << "You selected: Waiter" << endl;
             break;
         }
@@ -30,8 +25,9 @@ void startingMessages(char* role) {
 }
 
 int main() {
-    char role[ROLEMAXSIZE];
+    char role;
     startingMessages(role);
+
 
     return 0;
 }
