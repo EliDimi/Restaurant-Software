b683bf82d593d27dd093cae5efa1ceeb51e1653d
EliDimi <2275eli@gmail.com>
2025-01-19 18:14:24 +0200

    Creating a header file with all constants

---
+++ Constants.h
@@ -0,0 +1,43 @@
+#pragma once
+
+const int MAXSIZE = 100;
+const int SIZEOFDATE = 11;
+
+
+const char MENU_FILE[] = "Menu.txt";
+const char ORDERS_FILE[] = "Past orders.txt";
+const char SORTED_ORDERS_FILE[] = "Past orders sorted.txt";
+const char REVENUE_FILE[] = "Revenues.txt";
+const char RECIPES_FILE[] = "Recipes.txt";
+const char WAREHOUSE_FILE[] = "Warehouse.txt";
+
+const char MANAGER_MENU_OPTIONS[] =
+"Select an option:\n"
+"1) See menu\n"
+"2) Make an order\n"
+"3) Cancel an order\n"
+"4) See past orders\n"
+"5) See past orders sorted\n"
+"6) See how much products are left\n"
+"7) Write-off a product\n"
+"8) Stock a product\n"
+"9) See daily revenue\n"
+"10) Generate a daily revenue\n"
+"11) See all revenues from a date\n"
+"12) Add a new item in the menu\n"
+"13) Remove an item from the menu\n"
+"14) See all options\n"
+"15) Exit\n";
+
+const char WAITER_MENU_OPTIONS[] =
+"Select an option:\n"
+"1) See menu\n"
+"2) Make an order\n"
+"3) Cancel an order\n"
+"4) See past orders\n"
+"5) See past orders list\n"
+"6) See daily revenue\n"
+"7) See all options\n"
+"8) Exit\n";
+
+const char SEPARATOR[] = "---------------------------------";
